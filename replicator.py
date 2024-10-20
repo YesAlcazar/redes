6d7e5e53caaf05a05ac1100429f00b8c9797aa02
Eder Alcazar <ederalcazar@gmail.com>
2024-10-20 19:50:23 -0300

    new functions

--- replicator.py
+++ replicator.py
@@ -1,6 +1,23 @@
 import serial
 import time
 import os
+import threading
+
+
+# Função para replicar mensagens entre portas seriais
+def repply_all(msg : bytes, serial_ports : list, sender_port : serial.Serial = None) -> None:
+    if sender_port:
+        for serial_port in serial_ports:
+            if serial_port != sender_port:
+                serial_port.write(msg)
+                print(f"Enviando mensagem de {sender_port.name} para {serial_port.name}: {msg}")
+    else:
+        for serial_port in serial_ports:
+            serial_port.write(msg)
+            print(f"Enviando mensagem de {serial_port.name}: {msg}")
+
+def create_serial_ports(ports : list) -> list:
+    return list(serial.Serial(f"/dev/pts/{port}", timeout=1) for port in ports)
 
 # Replicador de mensagens entre portas seriais
 if __name__ == '__main__':
@@ -9,19 +26,19 @@ if __name__ == '__main__':
     serial_ports = list(serial.Serial(f"/dev/pts/{port}", timeout=1) for port in ports)
     for serial_port in serial_ports:
         print(f"Conectado à porta: {serial_port.name}")
+    print("Digite 'exit' em qualquer porta para encerrar a replicação de mensagens.")
     #Modo de operação de replicação de mensagens
     continue_operation = True
     while continue_operation:
         for in_port in serial_ports:
-            if in_port.in_waiting:
+            if in_port.in_waiting > 0:
                 msg = in_port.read(in_port.in_waiting)
                 continue_operation = msg != b'exit'
-                for out_port in serial_ports:
-                    if in_port != out_port:
-                        out_port.write(msg)
-                        print(f"Enviando mensagem de {in_port.name} para {out_port.name}: {msg}")
+                repply_all(msg, serial_ports, in_port)
         time.sleep(0.1)
     # Fechamento das portas seriais                
     for serial_port in serial_ports:
         print(f"Fechando porta: {serial_port.name}")
         serial_port.close()
+
+
