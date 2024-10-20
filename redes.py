541cfa2af73135f10791b73d6838a713b1457d09
Eder Alcazar <ederalcazar@gmail.com>
2024-10-20 15:53:28 -0300

    Update redes.py

--- redes.py
+++ redes.py
@@ -1,4 +1,13 @@
 import serial
 import time
 
+if __init__ == '__main__':
+    # Configuração da porta serial
+    ports = list(map(int,input("Digite as portas seriais: ").split()))
+    serial_ports = list(serial.Serial('/dev/pts/{port}', timeout=1) for port in ports)
+    for serial_port in serial_ports:
+        print(f"Conectado à porta: {serial_port.name}")
 
+    for serial_port in serial_ports:
+        print(f"Fechando porta: {serial_port.name}")
+        serial_port.close()
